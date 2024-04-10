0424bec9e340f9430cce972fefcc55facacad085
greensongavin <gavingreenson@icloud.com>
2024-04-10 19:24:50 +0800

    add: video split

--- scripts/panda70m/splitting/gen_video_list.py
+++ scripts/panda70m/splitting/gen_video_list.py
@@ -10,11 +10,14 @@ if __name__ == "__main__":
     parser.add_argument("--output_txt", type=str, default=None)
     args = parser.parse_args()
 
-    print(os.path.dirname(args.dataset_dir))
+    dataset_name = os.path.basename(args.dataset_dir)
+    root = os.path.dirname(args.dataset_dir)
+    if args.output_txt is None:
+        args.output_txt = os.path.join(root, f"{dataset_name}.txt")
 
+    file_names = []
     for root, dirs, files in os.walk(args.dataset_dir, topdown=False):
-        for name in files:
-            print(os.path.join(root, name))
-        for name in dirs:
-            print(os.path.join(root, name))
-        exit()
+        file_names.extend(file_names)
+
+    with open(args.output_txt, "w") as f:
+        f.writelines(file_names)
