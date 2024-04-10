b72742b93d754730fe874edb504624594d5129d7
greensongavin <gavingreenson@icloud.com>
2024-04-10 11:59:31 +0800

    update: check datasets

--- check_env/check_datasets.py
+++ check_env/check_datasets.py
@@ -21,15 +21,15 @@ os.makedirs(save_dir, exist_ok=True)
 
 if download:
     # 1. Download, error: 8,9,10,12,13,15,18
-    # data_files = ["pixabay.tar.gz.%02d" % x for x in [8, 9, 10, 12, 13, 15, 18]]
-    data_files = ["pexels.tar.gz.%02d" % x for x in range(6)]
+    data_files = ["pixabay.tar.gz.%02d" % x for x in [18]]
+    # data_files = ["pexels.tar.gz.%02d" % x for x in range(6)]
     target_dataset = load_dataset(
         path=hf_repo_name,
         cache_dir=cache_dir,
         num_proc=None,
         token="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",
         data_files=data_files,
-        # download_mode="force_redownload",
+        download_mode="force_redownload",
     )
     print(f"[Check Datasets] Dataset loaded to cache={cache_dir}.")
     exit()
