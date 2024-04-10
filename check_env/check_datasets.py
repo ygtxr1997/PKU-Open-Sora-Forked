634b7cac171b03a315c42d8b27a3a9de177427d2
greensongavin <gavingreenson@icloud.com>
2024-04-10 13:49:39 +0800

    update: check dataset

--- check_env/check_datasets.py
+++ check_env/check_datasets.py
@@ -7,7 +7,7 @@ download = True  # in [True, False]
 
 if target == "opensora":
     hf_repo_name = "LanguageBind/Open-Sora-Plan-v1.0.0"
-    cache_dir = "/public/home/201810101923/datasets/opensora/dataset_v1.0.0"
+    cache_dir = "/public/home/201810101923/datasets/opensora/dataset_v1.0.0_tmptest"
     save_dir = cache_dir + "_sorted"
 elif target == "imdb":
     hf_repo_name = "stanfordnlp/imdb"
@@ -20,6 +20,11 @@ os.makedirs(cache_dir, exist_ok=True)
 os.makedirs(save_dir, exist_ok=True)
 
 if download:
+
+    from huggingface_hub import hf_hub_download
+
+    hf_hub_download(repo_id=hf_repo_name, filename="mixkit.tar.gz", cache_dir=cache_dir)
+
     # 1. Download, error: 8,9,10,12,13,15,18
     # data_files = ["pixabay.tar.gz.%02d" % x for x in [18]]
     # data_files = ["pexels.tar.gz.%02d" % x for x in range(6)]
@@ -30,7 +35,7 @@ if download:
         # token="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",
         # data_files=data_files,
         # download_mode="force_redownload",
-        verification_mode="all_checks"
+        # verification_mode="all_checks"
     )
     print(f"[Check Datasets] Dataset loaded to cache={cache_dir}.")
     exit()
