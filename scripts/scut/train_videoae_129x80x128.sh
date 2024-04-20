e97051984ef22c6a4a6bc1ab4066987a91b42dad
greensongavin <gavingreenson@icloud.com>
2024-04-20 18:00:19 +0800

    add: train 129x80x128

--- scripts/scut/train_videoae_129x80x128.sh
+++ scripts/scut/train_videoae_129x80x128.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=opensora_train
 #SBATCH --partition=gpuA800
-#SBATCH --nodes=8
+#SBATCH --nodes=6
 #SBATCH --exclude=gpu[1]
 #SBATCH --ntasks-per-node=1          # crucial - only 1 task per dist per node!
 #SBATCH --cpus-per-task=64           # number of cores per tasks
