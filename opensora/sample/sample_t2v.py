3ffd579af38ace95532326fa56236058ff2a84a2
greensongavin <gavingreenson@icloud.com>
2024-04-25 16:23:11 +0800

    update: train 257x80x128

--- opensora/sample/sample_t2v.py
+++ opensora/sample/sample_t2v.py
@@ -177,7 +177,7 @@ if __name__ == "__main__":
     parser.add_argument("--version", type=str, default='65x512x512',
                         choices=['65x512x512', '65x256x256', '17x256x256',
                                  '17x288x512', '17x144x256', '17x72x128',
-                                 '129x80x128',
+                                 '129x80x128', '257x80x128',
                                  ])
     parser.add_argument("--sample_size", type=str, default=None)
     parser.add_argument("--ae", type=str, default='CausalVAEModel_4x8x8')
