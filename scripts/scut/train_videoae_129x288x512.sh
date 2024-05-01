dc682d8151f25242699ae4bef412eecf2364d03d
greensongavin <gavingreenson@icloud.com>
2024-05-02 01:35:01 +0800

    add: panda70m 129x288x512

--- scripts/scut/train_videoae_129x288x512.sh
+++ scripts/scut/train_videoae_129x288x512.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=opensora_train
 #SBATCH --partition=gpuA800
-#SBATCH --nodes=1
+#SBATCH --nodes=8
 #SBATCH --exclude=gpu[1]
 #SBATCH --ntasks-per-node=1          # crucial - only 1 task per dist per node!
 #SBATCH --cpus-per-task=64           # number of cores per tasks
@@ -89,9 +89,9 @@ srun --jobid $SLURM_JOBID bash -c 'accelerate launch \
   --wh_ratio "16:9" \
   --gradient_checkpointing \
   --attention_mode xformers \
-  --train_batch_size=1 \
+  --train_batch_size=2 \
   --dataloader_num_workers 6 \
-  --gradient_accumulation_steps=2 \
+  --gradient_accumulation_steps=1 \
   --max_train_steps=1000000 \
   --learning_rate=2e-05 \
   --lr_scheduler="constant" \
