2b1e67cc2b49ff6de5213e408567ddc06659b8e2
greensongavin <gavingreenson@icloud.com>
2024-05-02 12:41:07 +0800

    add: panda70m 129x288x512

--- opensora/sample/pipeline_videogen.py
+++ opensora/sample/pipeline_videogen.py
@@ -758,6 +758,7 @@ class VideoGenPipeline(DiffusionPipeline):
         return VideoPipelineOutput(video=video)
 
     def decode_latents(self, latents):
+        self.vae = self.vae.to("cpu")
         video = self.vae.decode(latents)
         # video = self.vae.decode(latents / 0.18215)
         # video = rearrange(video, 'b c t h w -> b t c h w').contiguous()
