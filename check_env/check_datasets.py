51de45175b5daf9af405fbc2a63cab89d62a7513
greensongavin <gavingreenson@icloud.com>
2024-05-08 01:02:05 +0800

    debug: webvid dataset

--- check_env/check_datasets.py
+++ check_env/check_datasets.py
@@ -169,8 +169,8 @@ def check_batch():
 
     # 4. WebVid Dataset
     from opensora.dataset.webvid_datasets import WebVidHFWebDataset
-    WEBVID_DIR = "/exthome/future-technology-college-data/202321063560/webvid_data/webvid_train_data"
-    # WEBVID_DIR = "/public/home/201810101923/datasets/webvid/data_demo"
+    # WEBVID_DIR = "/exthome/future-technology-college-data/202321063560/webvid_data/webvid_train_data"
+    WEBVID_DIR = "/public/home/201810101923/datasets/webvid/data_demo"
     tokenizer = AutoTokenizer.from_pretrained(
             args.text_encoder_name, cache_dir=args.cache_dir)
     train_dataset = WebVidHFWebDataset(
