e29dfe0945ba0d037f64e78a78ec23b80b1b1539
greensongavin <gavingreenson@icloud.com>
2024-05-10 01:21:15 +0800

    debug: slurm multi nodes

--- check_env/slurm_debug.sh
+++ check_env/slurm_debug.sh
@@ -29,11 +29,11 @@ export NCCL_NET=IB
 ######################
 
 export LAUNCHER="accelerate launch \
+    --config_file scripts/accelerate_configs/deepspeed_zero2_config.yaml  \
     --num_processes $((SLURM_NNODES * GPUS_PER_NODE)) \
     --num_machines $SLURM_NNODES \
     --main_process_ip ${MASTER_ADDR} \
     --main_process_port ${MASTER_PORT} \
-    --machine_rank \$SLURM_PROCID  \
     "
 export PYTHONPATH=${PWD}
 export DATA_PATH="/public/home/201810101923/datasets/opensora/dataset_v1.0.0_tmptest_sorted/sharegpt4v_path_cap_64x512x512.json"
@@ -79,6 +79,7 @@ export SCRIPT_ARGS=" \
     --output_dir ${OUTPUT_DIR} \
     --allow_tf32 \
     --pretrained ${PRETRAINED_MODEL_PT} \
+    --use_deepspeed \
     --model_max_length 300 \
     --use_image_num 0 \
     --enable_tiling \
@@ -95,5 +96,10 @@ export SCRIPT_ARGS=" \
     "
 
 # This step is necessary because accelerate launch does not handle multiline arguments properly
-export CMD="$LAUNCHER $SCRIPT $SCRIPT_ARGS"
-srun --jobid $SLURM_JOBID bash -c "$CMD"
+#export CMD="$LAUNCHER $SCRIPT $SCRIPT_ARGS"
+#srun --jobid $SLURM_JOBID bash -c "$CMD"
+All_ADDR=($(scontrol show hostnames $SLURM_JOB_NODELIST))
+for mrank in $(seq 0 $((SLURM_NNODES - 1))) do
+  echo "$mrank address"=${All_ADDR[mrank]}
+  srun $SRUN_ARGS -w ${All_ADDR[mrank]} bash -c "$LAUNCHER --machine_rank $mrank $SCRIPT $SCRIPT_ARGS" &
+done
