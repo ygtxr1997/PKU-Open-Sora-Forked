8c245b5dea70d776bf9d7d6e2af6696886711f36
greensongavin <gavingreenson@icloud.com>
2024-05-10 01:55:42 +0800

    debug: slurm multi nodes

--- check_env/slurm_debug.sh
+++ check_env/slurm_debug.sh
@@ -30,14 +30,14 @@ export NCCL_NSOCKS_PERTHREAD=4
 export NCCL_SOCKET_NTHREADS=2
 export NCCL_MIN_CHANNELS=32
 
-#export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
-#export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
-#echo "MASTER_PORT"=$MASTER_PORT
-#echo "WORLD_SIZE="$WORLD_SIZE
-#
-#master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-#export MASTER_ADDR=$master_addr
-#echo "MASTER_ADDR="$MASTER_ADDR
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
+echo "MASTER_PORT"=$MASTER_PORT
+echo "WORLD_SIZE="$WORLD_SIZE
+
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+echo "MASTER_ADDR="$MASTER_ADDR
 ######################
 
 #export LAUNCHER="accelerate launch \
@@ -115,11 +115,11 @@ export SCRIPT_ARGS=" \
     "
 
 # This step is necessary because accelerate launch does not handle multiline arguments properly
-#export CMD="$LAUNCHER $SCRIPT $SCRIPT_ARGS"
-#srun --jobid $SLURM_JOBID bash -c "$CMD"
-All_ADDR=($(scontrol show hostnames $SLURM_JOB_NODELIST))
-for mrank in $(seq 0 $((SLURM_NNODES - 1)))
-do
-echo "$mrank address"=${All_ADDR[mrank]}
-srun --jobid $SLURM_JOBID -w ${All_ADDR[mrank]} bash -c "$LAUNCHER $SCRIPT $SCRIPT_ARGS" &
-done
+export CMD="$LAUNCHER $SCRIPT $SCRIPT_ARGS"
+srun --jobid $SLURM_JOBID bash -c "$CMD"
+#All_ADDR=($(scontrol show hostnames $SLURM_JOB_NODELIST))
+#for mrank in $(seq 0 $((SLURM_NNODES - 1)))
+#do
+#echo "$mrank address"=${All_ADDR[mrank]}
+#srun --jobid $SLURM_JOBID -w ${All_ADDR[mrank]} bash -c "$LAUNCHER $SCRIPT $SCRIPT_ARGS" &
+#done
