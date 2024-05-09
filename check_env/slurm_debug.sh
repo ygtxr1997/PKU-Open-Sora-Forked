855c175b323722c69e6d01c03d36ba5982073d85
greensongavin <gavingreenson@icloud.com>
2024-05-09 23:21:19 +0800

    debug: slurm multi nodes

--- check_env/slurm_debug.sh
+++ check_env/slurm_debug.sh
@@ -28,6 +28,20 @@ export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export NCCL_NET=IB
 ######################
 
+export PYTHONPATH=${PWD}
+export DATA_PATH="/public/home/201810101923/datasets/opensora/dataset_v1.0.0_tmptest_sorted/sharegpt4v_path_cap_64x512x512.json"
+export REPLACE_ROOT="/public/home/201810101923/datasets/opensora/dataset_v1.0.0_tmptest_sorted"
+export MODEL_CACHE_DIR="/public/home/201810101923/models/opensora/v1.0.0"
+export PRETRAINED_MODEL_PT="/public/home/201810101923/models/opensora/v1.0.0_sorted/panda70m_129x144x256/checkpoint-96000/model/diffusion_pytorch_model.safetensors"
+export INTERNVID_DIR="/exthome/future-technology-college-data/Internvid_dataset/InternVid-10M-FLT-clip"
+export INTERNVID_META="/exthome/future-technology-college-data/Internvid_dataset/InternVid-10M-flt-clips1.jsonl"
+export PANDA70M_DIR="/public/home/201810101923/datasets/panda70m/clips_0"
+export PANDA70M_META="/public/home/201810101923/datasets/panda70m/panda70m_training_clips_0.csv"
+export WEBVID_DIR="/exthome/future-technology-college-data/202321063560/webvid_data/webvid_train_data"
+export WEBVID_LATENT_DIR="/public/home/201810101923/datasets/webvid/latents"
+export WEBVID_LATENT_META="/public/home/201810101923/datasets/webvid/latents_129x288x512/latents_meta_all.csv"
+export OUTPUT_DIR="out_panda70m_129x288x512"
+export VIDEO_FOLDER="/remote-home1/dataset/data_split_tt"  # not used
 export LAUNCHER="accelerate launch \
     --config_file scripts/accelerate_configs/deepspeed_zero2_config.yaml  \
     --num_processes $((SLURM_NNODES * GPUS_PER_NODE)) \
@@ -40,13 +54,13 @@ export SCRIPT="check_env/check_multi_nodes.py"
 export SCRIPT_ARGS=' \
     --model LatteT2V-XL/122 \
     --text_encoder_name DeepFloyd/t5-v1_1-xxl \
-    --cache_dir "."  \
+    --cache_dir ${MODEL_CACHE_DIR}  \
     --dataset webvid_latent \
     --ae CausalVAEModel_4x8x8 \
     --ae_path CausalVAEModel_4x8x8 \
-    --data_path "." \
-    --replace_root "."  \
-    --video_folder "." \
+    --data_path ${DATA_PATH} \
+    --replace_root ${REPLACE_ROOT}  \
+    --video_folder ${VIDEO_FOLDER} \
     --sample_rate 1 \
     --num_frames 129 \
     --max_image_size 512 \
@@ -63,9 +77,9 @@ export SCRIPT_ARGS=' \
     --mixed_precision="bf16" \
     --report_to="wandb" \
     --checkpointing_steps=500 \
-    --output_dir="." \
+    --output_dir=${OUTPUT_DIR} \
     --allow_tf32 \
-    --pretrained "." \
+    --pretrained ${PRETRAINED_MODEL_PT} \
     --use_deepspeed \
     --model_max_length 300 \
     --use_image_num 0 \
@@ -73,6 +87,13 @@ export SCRIPT_ARGS=' \
     --tracker_project_name scut_opensora \
     --tracker_run_name opensora512  \
     --resume_from_checkpoint "latest"  \
+    --internvid_meta ${INTERNVID_META}  \
+    --internvid_dir ${INTERNVID_DIR}  \
+    --panda70m_meta ${PANDA70M_META}  \
+    --panda70m_dir ${PANDA70M_DIR}  \
+    --is_video_latent  \
+    --webvid_meta ${WEBVID_LATENT_META}  \
+    --webvid_dir ${WEBVID_LATENT_DIR}
     '
 
 # This step is necessary because accelerate launch does not handle multiline arguments properly
