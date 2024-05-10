864cf5ad409ad603f8207d6616bcdfee7a1d9cef
greensongavin <gavingreenson@icloud.com>
2024-05-10 14:23:32 +0800

    debug: slurm multi nodes

--- check_env/slurm_debug.sh
+++ check_env/slurm_debug.sh
@@ -144,6 +144,21 @@ srun accelerate launch \
   --machine_rank '$SLURM_PROCID' \
   $SCRIPT $SCRIPT_ARGS
 
+All_ADDR=($(scontrol show hostnames $SLURM_JOB_NODELIST))
+for mrank in $(seq 0 $((SLURM_NNODES - 1)))
+do
+echo "$mrank address"=${All_ADDR[mrank]}
+srun --jobid $SLURM_JOBID -w ${All_ADDR[mrank]} bash -c "srun accelerate launch \
+  --config_file scripts/accelerate_configs/acc_fsdp_config.yaml \
+  --num_processes $((SLURM_NNODES * GPUS_PER_NODE)) \
+  --num_machines $SLURM_NNODES \
+  --main_process_ip ${MASTER_ADDR} \
+  --main_process_port ${MASTER_PORT} \
+  --machine_rank $mrank \
+  $SCRIPT $SCRIPT_ARGS
+  " &
+done
+
 ##srun accelerate launch \
 #  --config_file scripts/accelerate_configs/deepspeed_zero2_config.yaml \
 #  --num_processes $((SLURM_NNODES * GPUS_PER_NODE)) \
