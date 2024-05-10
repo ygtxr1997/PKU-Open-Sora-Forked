27bbcfc3a6edaccfa80ff01a11e98a4d9022d466
greensongavin <gavingreenson@icloud.com>
2024-05-10 10:06:54 +0800

    debug: slurm multi nodes

--- check_env/slurm_debug.sh
+++ check_env/slurm_debug.sh
@@ -138,7 +138,7 @@ deepspeed --hostfile hostfile \
   --num_gpus 32 \
   --launcher SLURM \
   $SCRIPT $SCRIPT_ARGS \
-  --deepspeed scripts/accelerate_configs/zero2.json
+  --deepspeed
 
 #srun accelerate launch \
 #  --config_file scripts/accelerate_configs/deepspeed_zero2_config.yaml \
