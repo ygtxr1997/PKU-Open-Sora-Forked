284883a7022ff32f9ee68601954b4b55f8520595
greensongavin <gavingreenson@icloud.com>
2024-05-10 11:27:49 +0800

    debug: slurm multi nodes

--- check_env/slurm_debug.sh
+++ check_env/slurm_debug.sh
@@ -39,15 +39,6 @@ export NCCL_NET=IB
 export NCCL_NSOCKS_PERTHREAD=4
 export NCCL_SOCKET_NTHREADS=2
 export NCCL_MIN_CHANNELS=32
-
-export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
-export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
-echo "MASTER_PORT"=$MASTER_PORT
-echo "WORLD_SIZE="$WORLD_SIZE
-
-master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-export MASTER_ADDR=$master_addr
-echo "MASTER_ADDR="$MASTER_ADDR
 ######################
 
 #export LAUNCHER="accelerate launch \
