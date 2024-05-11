27aa2c8f0df6da6c1695448550bdfe1ad05a5b64
greensongavin <gavingreenson@icloud.com>
2024-05-11 15:23:10 +0800

    debug: webvid latent input

--- scripts/scut/slurm_gpu.sh
+++ scripts/scut/slurm_gpu.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=scut_gpu_task
 #SBATCH --partition=gpuA800
 #SBATCH --nodes=1
-#SBATCH --exclude=gpu[10,15-17]
+#SBATCH --exclude=gpu[10,15-19]
 #SBATCH --ntasks-per-node=1          # crucial - only 1 task per dist per node!
 #SBATCH --cpus-per-task=16           # number of cores per tasks
 #SBATCH --mem=50000MB                # memory
