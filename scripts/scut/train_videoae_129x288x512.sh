3459052e890e0bde7491d702afa48699b481798c
greensongavin <gavingreenson@icloud.com>
2024-05-11 11:56:43 +0800

    debug: webvid latent input

--- scripts/scut/train_videoae_129x288x512.sh
+++ scripts/scut/train_videoae_129x288x512.sh
@@ -119,7 +119,8 @@ srun --jobid $SLURM_JOBID bash -c 'accelerate launch \
   --panda70m_dir ${PANDA70M_DIR}  \
   --is_video_latent  \
   --webvid_meta ${WEBVID_LATENT_META}  \
-  --webvid_dir ${WEBVID_LATENT_DIR}
+  --webvid_dir ${WEBVID_LATENT_DIR}  \
+  --enable_tracker
   '
 
 echo "DONE"
