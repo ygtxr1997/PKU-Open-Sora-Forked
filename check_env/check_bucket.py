15de98d94c7cf992eff66233f36d74da2303b25c
greensongavin <gavingreenson@icloud.com>
2024-05-20 15:16:38 +0800

    add: bucket and aspect ratio settings

--- check_env/check_bucket.py
+++ check_env/check_bucket.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 
 import torch
@@ -16,7 +17,7 @@ def main():
 
     for i in range(len(num_frames)):
         t, h, w = num_frames[i], heights[i], widths[i]
-        bucket_id = bucket.get_bucket_id(t, h, w)
+        bucket_id = bucket.get_bucket_id(t, h, w, seed=time.time())
         print(f"({t}, {h}, {w}): {bucket_id}")
 
 
