dbb6fa52df4eca84f54701bc894ed17fd13e391f
greensongavin <gavingreenson@icloud.com>
2024-06-02 16:57:02 +0800

    update: hku sampling

---
+++ scripts/hku/sample_video.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -x -e
+
+# Set SLURM-like env params by hand
+export SLURM_NNODES=1
+export SLURM_PROCID=0
+
+export NUM_GPUS=8
+
+export OMP_NUM_THREADS=4
+
+# Make sure another job doesnt use same port, here using random number
+export MASTER_PORT=$((RANDOM % (19000 - 11000 + 1) + 11000))
+export MASTER_ADDR="127.0.0.1"
+
+RANK=${SLURM_PROCID}
+
+echo "START TIME: $(date)"
+
+export NCCL_NET=IB
+
+# GPU nodes have no network
+wandb offline
+
+
+# please copy (or make a soft link by 'ln -s') the pretrained pipline to the current dir for more stable pipeline loading
+# also remember to set "wandb offline" before training, and use syncwandb.sh to upload to wandb
+export PYTHONPATH=${PWD}
+export CACHE_DIR="/home/geyuan/pretrained/opensora/v1.0.0"
+export PROMPT_LIST="examples/demo.txt"
+export TRAIN_SIZE="v257x288x512"
+export SAMPLE_SIZE="257x288x512"
+export TRAIN_STEPS="58700"
+export CKPT_PATH="/home/geyuan/pretrained/opensora/out_webvidlatent_${TRAIN_SIZE}_qknorm_rope/checkpoint-${TRAIN_STEPS}/model/diffusion_pytorch_model.safetensors"
+export OUTPUT_DIR="./sample_videos/demo_webvidlatent${TRAIN_SIZE}_qknorm_rope_${TRAIN_STEPS}_${SAMPLE_SIZE}"
+bash -c 'accelerate launch \
+  --config_file scripts/accelerate_configs/deepspeed_zero2_config.yaml \
+  --num_processes $(($NUM_GPUS * $SLURM_NNODES)) --num_machines $SLURM_NNODES --machine_rank $SLURM_PROCID \
+  --main_process_ip $MASTER_ADDR --main_process_port $MASTER_PORT \
+  opensora/sample/sample_t2v.py \
+  --model_path LanguageBind/Open-Sora-Plan-v1.0.0 \
+  --ckpt_path ${CKPT_PATH}  \
+  --cache_dir ${CACHE_DIR} \
+  --text_encoder_name DeepFloyd/t5-v1_1-xxl \
+  --text_prompt ${PROMPT_LIST} \
+  --ae CausalVAEModel_4x8x8 \
+  --version ${TRAIN_SIZE} \
+  --sample_size ${SAMPLE_SIZE} \
+  --save_img_path ${OUTPUT_DIR} \
+  --fps 24 \
+  --guidance_scale 7.5 \
+  --num_sampling_steps 250 \
+  --enable_tiling
+  '
+
+echo "DONE"
---
+++ scripts/scut/download_files.py
@@ -0,0 +1,9 @@
+from huggingface_hub import hf_hub_download
+
+
+hf_hub_download(
+    repo_id="ygtxr1997/t2v_forked_pku_opensora",
+    filename="config.json",
+    local_dir="/home/geyuan/pretrained/opensora/out_webvidlatent_v257x288x512_qknorm_rope/checkpoint-58700/model/",
+    token="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"
+)
